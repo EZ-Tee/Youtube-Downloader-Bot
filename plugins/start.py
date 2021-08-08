15989dce3659793cbbbb398325b64a956a8f532c
Ez-Tee <81809747+EZ-Tee@users.noreply.github.com>
2021-08-08 13:23:14 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,20 +1,25 @@
-from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery, InlineQuery, Update
 
 
 @Client.on_message(Filters.command(["start"]), group=-2)
 async def start(client, message):
     # return
-    img = "https://telegra.ph/file/af86973849bc43cc8e3ce.jpg"
+    
     joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("Channel", url="https://t.me/aryan_bots")],
+        [InlineKeyboardButton("Channel", callback_data="help")],
         [InlineKeyboardButton(
             "Report Bugs 😊", url="https://t.me/aryanvikash")]
     ])
     welcomed = "Hey <b>{message.from_user.first_name}</b>\n/help for More info"
-    await message.reply_photo(
-img)
 await message.reply_text(
 welcomed,
 reply_markup=joinButton)
+
+@Client.on_callback_query()
+async def button(Client, update):
+      cb_data = update.data
+      if "help" in cb_data:
+        await update.message.delete()
+        await help(Client, update.message)
 
     raise StopPropagation
