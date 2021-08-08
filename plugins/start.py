4296a58005f77e8dfa4c95f14642b02646346fde
Ez-Tee <81809747+EZ-Tee@users.noreply.github.com>
2021-08-08 12:04:34 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -4,11 +4,15 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 @Client.on_message(Filters.command(["start"]), group=-2)
 async def start(client, message):
     # return
+    img = "https://telegra.ph/file/af86973849bc43cc8e3ce.jpg"
     joinButton = InlineKeyboardMarkup([
         [InlineKeyboardButton("Channel", url="https://t.me/aryan_bots")],
         [InlineKeyboardButton(
             "Report Bugs 😊", url="https://t.me/aryanvikash")]
     ])
     welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
-    await message.reply_text(welcomed, reply_markup=joinButton)
+    await message.reply_photo(
+img,
+welcomed,
+reply_markup=joinButton)
     raise StopPropagation
