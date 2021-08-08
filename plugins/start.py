0ce9672f71d78e595ea77de89a833e49299a81a0
Ez-Tee <81809747+EZ-Tee@users.noreply.github.com>
2021-08-08 17:38:24 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -4,13 +4,13 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 @Client.on_message(Filters.command(["start"]), group=-2)
 async def start(client, message):
     # return
-    sticke = "CAACAgEAAxkBAAIeSGELTx2IPwLHPV_aGClOIh2bVzdTAALjAANRKQ05F6bHecKQ5JseBA"
+    sticke = "https://telegra.ph/file/af86973849bc43cc8e3ce.jpg"
     joinButton = InlineKeyboardMarkup([
         [InlineKeyboardButton("Channel ⚒", url="https://t.me/ez_tee_youtibe")],
         [InlineKeyboardButton("Feedback👩‍🔧", url="https://t.me/eztee_chat")]
     ])
     
-    await message.reply_sticker(
+    await message.reply_photo(
         sticke,
         reply_markup=joinButton)
     raise StopPropagation
